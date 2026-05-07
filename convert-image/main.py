9a0b80d97576f830127d363b242158dcf49ca15a
flyingfig21 <cehgamer21@gmail.com>
2026-05-07 15:09:06 -0700

    Add comments to convert-image main.py

--- convert-image/main.py
+++ convert-image/main.py
@@ -10,15 +10,18 @@ OUTPUT_BUCKET = "converted-image-bucket"
 
 @functions_framework.cloud_event
 def convert_image(cloud_event):
+    #Decode the Pub/Sub message
     message = cloud_event.data["message"]["data"]
     message_decoded = base64.b64decode(message).decode("utf-8")
     message_json = json.loads(message_decoded)
+    #Get the image name and bucket name, then get the image
     file_name = message_json["name"]
     bucket_name = message_json["bucket"]
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(file_name)
     image = blob.download_as_bytes()
 
+    #Open the image, convert it to JPEG format, and place it in the converted image bucket
     with Image.open(io.BytesIO(image)) as img:
         img = img.convert("RGB")
         output_buffer = io.BytesIO()
