e8eef03138c981b1ed68bf40794aed7ab6ba81cf
flyingfig21 <cehgamer21@gmail.com>
2026-05-07 15:08:22 -0700

    Add comments to unzip-file main.py

--- unzip-file/main.py
+++ unzip-file/main.py
@@ -10,14 +10,17 @@ OUTPUT_BUCKET = "unconverted-image-bucket"
 
 @functions_framework.cloud_event
 def process_zip(cloud_event):
+    #Decode the Pub/Sub message
     message = cloud_event.data["message"]["data"]
     message_decoded = base64.b64decode(message).decode("utf-8")
     message_json = json.loads(message_decoded)
+    #Get the file name and bucket name, then get the zip file
     file_name = message_json["name"]
     bucket_name = message_json["bucket"]
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(file_name)
     zip_file = blob.download_as_bytes()
+    #Unzip the file and place the contents in the unconverted image bucket
     with zf.ZipFile(io.BytesIO(zip_file), 'r') as zip:
         for info in zip.infolist():
             if info.is_dir():
